896bee3fa90761ddbe840ce9fbe7ae4119f062b4
Frederick Noe <fdn2@calvin.edu>
2019-09-20 15:43:42 -0400

    lab01 and lab02

---
+++ LAB01/android/app/src/main/java/edu/calvin/cs262/fdn2/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.calvin.cs262.fdn2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstnceState);
+        setContentView(R.layout.activity_main);
+    }
+}
